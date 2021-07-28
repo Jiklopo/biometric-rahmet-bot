5a6fa444a3c4b9a0c15b009e67c81ad2a348a0a5
Timur <t_khaimovich@outlook.com>
2021-07-28 19:27:01 +0600

    returned echo handler

--- bot/bot.py
+++ bot/bot.py
@@ -1,9 +1,16 @@
 import os
 from dotenv import load_dotenv
-from aiogram import Bot, Dispatcher
+
+from aiogram.types import ContentType
+from aiogram import Bot, Dispatcher, types
 
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
 
 bot = Bot(TOKEN)
 dp = Dispatcher(bot)
+
+
+@dp.message_handler(content_types=[ContentType.TEXT])
+async def echo(message: types.Message):
+    await message.reply(message.text)
