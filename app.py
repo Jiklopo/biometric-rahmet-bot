9599c72e7d51ddf7514906461f14a4b59cafec4d
Timur <t_khaimovich@outlook.com>
2021-07-28 19:05:09 +0600

    moved all start related to app

---
+++ app.py
@@ -0,0 +1,39 @@
+import os
+from dotenv import load_dotenv
+from urllib.parse import urljoin
+from aiogram.utils import executor
+from aiohttp import web
+from aiogram.dispatcher.webhook import get_new_configured_app
+
+from bot.bot import bot, dp
+
+load_dotenv()
+ENV = os.getenv('ENV')
+TOKEN = os.getenv('TOKEN')
+APP_URL = os.getenv('APP_URL', '')
+
+WEBHOOK_URL_PATH = 'webhook/' + TOKEN
+WEBHOOK_URL = urljoin(APP_URL, WEBHOOK_URL_PATH)
+
+
+def heroku_run():
+    bot.delete_webhook()
+    bot.set_webhook(WEBHOOK_URL)
+    app = get_new_configured_app(dispatcher=dp, path=WEBHOOK_URL_PATH)
+    web.run_app(app, host='0.0.0.0', port=os.getenv('PORT'))
+
+
+def local_run():
+    bot.delete_webhook()
+    print('Starting server...')
+    executor.start_polling(
+        dispatcher=dp,
+        on_startup=print('Server Started.')
+    )
+
+
+if __name__ == '__main__':
+    if ENV == 'HEROKU':
+        heroku_run()
+    elif ENV == 'DEV':
+        local_run()
--- bot/bot.py
+++ bot/bot.py
@@ -1,40 +1,9 @@
-import asyncio
 import os
-from urllib.parse import urljoin
-
-from aiogram.utils import executor
 from dotenv import load_dotenv
-from aiogram.types import ContentType
-from aiohttp import web
-from aiogram import Bot, Dispatcher, types
-from aiogram.dispatcher.webhook import get_new_configured_app
+from aiogram import Bot, Dispatcher
 
 load_dotenv()
-ENV = os.getenv('ENV')
 TOKEN = os.getenv('TOKEN')
-APP_URL = os.getenv('APP_URL', '')
-
-WEBHOOK_URL_PATH = 'webhook/' + TOKEN
-WEBHOOK_URL = urljoin(APP_URL, WEBHOOK_URL_PATH)
 
 bot = Bot(TOKEN)
 dp = Dispatcher(bot)
-
-
-def heroku_run():
-    bot.delete_webhook()
-    bot.set_webhook(WEBHOOK_URL)
-    app = get_new_configured_app(dispatcher=dp, path=WEBHOOK_URL_PATH)
-    web.run_app(app, host='0.0.0.0', port=os.getenv('PORT'))
-
-
-def local_run():
-    bot.delete_webhook()
-    executor.start_polling(dispatcher=dp)
-
-
-if __name__ == '__main__':
-    if ENV == 'HEROKU':
-        heroku_run()
-    else:
-        local_run()
