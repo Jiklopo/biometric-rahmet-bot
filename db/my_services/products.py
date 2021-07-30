6bf705c925d00a95453e31878f9b93ae70b6175c
Timur <t_khaimovich@outlook.com>
2021-07-30 16:10:46 +0600

    created products selectors and services

---
+++ db/my_services/products.py
@@ -0,0 +1,14 @@
+from sqlalchemy.orm import Session
+
+from db import engine
+from db.tables import Product, Order
+
+
+def create_product(*, name, price=None) -> Product:
+    with Session(engine) as session:
+        product = Product(name=name, price=price)
+        session.add(product)
+        session.commit()
+        session.refresh(instance=product)
+
+    return product
