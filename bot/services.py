03022c3b13137d2b3a9f84089f51f02da7ae13e8
Timur <t_khaimovich@outlook.com>
2021-07-30 18:20:22 +0600

    fixed notify service

--- bot/services.py
+++ bot/services.py
@@ -7,5 +7,5 @@ from db.tables import User
 async def notify_users(*, bot: Bot, author: User, notification_text: str):
     users = list_users()
     for user in users:
-        if user.telegram_id == author.telegram_id:
+        if user.telegram_id != author.telegram_id:
             await bot.send_message(user.telegram_id, notification_text)
