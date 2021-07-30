5e15bf95032489aba81fa11c164865e30958347b
Timur <t_khaimovich@outlook.com>
2021-07-30 17:07:18 +0600

    created bot service

---
+++ bot/services.py
@@ -0,0 +1,11 @@
+from aiogram import Bot
+
+from db.my_selectors.users import list_users
+from db.tables import User
+
+
+async def notify_users(*, bot: Bot, author: User, notification_text: str):
+    users = list_users()
+    for user in users:
+        if user.telegram_id == author.telegram_id:
+            await bot.send_message(user.telegram_id, notification_text)
