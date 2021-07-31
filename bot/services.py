d301e2f90429f3390cbd28389e0d9ec8fb105bd0
Timur <t_khaimovich@outlook.com>
2021-07-31 14:16:51 +0600

    created checker services

--- bot/services.py
+++ bot/services.py
@@ -1,7 +1,12 @@
-from aiogram import Bot
+from aiogram import Bot, types
 
 from db.my_selectors.users import list_users
+from db.states import UserStates
 from db.tables import User
+
+
+class WrongStateException(Exception):
+    pass
 
 
 async def notify_users(*, bot: Bot, author: User, notification_text: str):
@@ -9,3 +14,30 @@ async def notify_users(*, bot: Bot, author: User, notification_text: str):
     for user in users:
         if user.telegram_id != author.telegram_id:
             await bot.send_message(user.telegram_id, notification_text)
+
+
+def check_registration(*, msg: types.Message, user: User):
+    reply = None
+    if not user:
+        reply = 'Вы не зарегистрированы! Используйте команду /register'
+    if reply:
+        msg.reply(reply)
+        raise WrongStateException
+
+
+def check_kaspi(*, msg: types.Message, user: User):
+    reply = None
+    if user.state == UserStates.CREATED.value:
+        reply = 'Вы не указали номер Каспи! Отправьте его сюда.'
+    if reply:
+        msg.reply(reply)
+        raise WrongStateException
+
+
+def check_ordering(*, msg: types.Message, user: User):
+    reply = None
+    if user.state == UserStates.ORDERING.value:
+        reply = 'У вас есть активный заказ! Закройте его, используя /close.'
+    if reply:
+        msg.reply(reply)
+        raise WrongStateException
