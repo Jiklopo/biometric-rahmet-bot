86b1f55456effe33559bd06cc2acc31a768000e9
Timur <t_khaimovich@outlook.com>
2021-08-02 13:53:32 +0600

    fixed database_url for Heroku

--- db/__init__.py
+++ db/__init__.py
@@ -6,6 +6,6 @@ load_dotenv()
 DATABASE_URL = getenv('DATABASE_URL')
 
 if DATABASE_URL.startswith('postgres://'):
-    DATABASE_URL = DATABASE_URL.replace('postgres', 'postgres+psycopg2')
+    DATABASE_URL = DATABASE_URL.replace('postgres', 'postgresql+psycopg2')
 
 engine = sqlalchemy.create_engine(DATABASE_URL, echo=True, future=True)
