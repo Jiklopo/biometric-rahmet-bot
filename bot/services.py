cf8ff46acf70ca3244e49fd6e3ec2abe049ecb9e
Timur <t_khaimovich@outlook.com>
2021-08-02 11:25:44 +0600

    made services async

--- bot/services.py
+++ bot/services.py
@@ -9,6 +9,10 @@ class WrongStateException(Exception):
     pass
 
 
+class WrongChatException(Exception):
+    pass
+
+
 def update_order_message(*, bot: Bot, order: Order, text: str, inline_markup: types.InlineKeyboardMarkup = None):
     return bot.edit_message_text(
         text=text,
@@ -18,28 +22,40 @@ def update_order_message(*, bot: Bot, order: Order, text: str, inline_markup: ty
     )
 
 
-def check_registration(*, msg: types.Message, user: User):
+async def check_private(*, msg: types.Message):
+    if msg.chat.type != 'private':
+        await msg.reply('Эту команду можно использовать только в личном чате!')
+        raise WrongChatException
+
+
+async def check_group(*, msg: types.Message):
+    if msg.chat.type != 'group':
+        await msg.reply('Эта команда работает только в групповых чатах!')
+        raise WrongChatException
+
+
+async def check_registration(*, msg: types.Message, user: User):
     reply = None
     if not user:
         reply = 'Вы не зарегистрированы! Используйте команду /register'
     if reply:
-        msg.reply(reply)
+        await msg.reply(reply)
         raise WrongStateException
 
 
-def check_kaspi(*, msg: types.Message, user: User):
+async def check_kaspi(*, msg: types.Message, user: User):
     reply = None
     if user.state == UserStates.CREATED.value:
         reply = 'Вы не указали номер Каспи! Отправьте его сюда.'
     if reply:
-        msg.reply(reply)
+        await msg.reply(reply)
         raise WrongStateException
 
 
-def check_ordering(*, msg: types.Message, user: User):
+async def check_ordering(*, msg: types.Message, user: User):
     reply = None
     if user.state == UserStates.ORDERING.value:
         reply = 'У вас есть активный заказ! Закройте его, используя /close.'
     if reply:
-        msg.reply(reply)
+        await msg.reply(reply)
         raise WrongStateException
