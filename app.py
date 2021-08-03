b160658f020014af1ef4cd39e88173de6d9a69a8
Timur <t_khaimovich@outlook.com>
2021-08-03 11:12:23 +0600

    no need to delete webhook

--- app.py
+++ app.py
@@ -20,14 +20,10 @@ def heroku_run():
         await bot.delete_webhook()
         await bot.set_webhook(WEBHOOK_URL)
 
-    async def on_shutdown(dp):
-        await bot.delete_webhook()
-
     executor.start_webhook(
         dispatcher=dp,
         webhook_path=WEBHOOK_PATH,
         on_startup=on_startup,
-        on_shutdown=on_shutdown,
         skip_updates=True,
         host='0.0.0.0',
         port=os.getenv('PORT'),
